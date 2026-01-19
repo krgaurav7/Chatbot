11f058d650104718315914d1a0713cf5f31b1294
Gaurav Kumar <gk107174@gmail.com>
2026-01-19 10:46:32 +0530

    add chat streaming handler and chat streaming route

--- src/handlers/chat_handler.py
+++ src/handlers/chat_handler.py
@@ -1,6 +1,7 @@
 from src.agents.chat_agent.graph import create_chat_agent_graph_builder
 from langchain.messages import HumanMessage
 from src.agents.chat_agent.states.chat_agent_state import ChatAgentState
+from typing import Iterator
 
 graph = create_chat_agent_graph_builder()
 
@@ -23,6 +24,28 @@ def chat_agent_handler(thread_id : str , message : str) -> ChatAgentState:
                   }
                }
       ) # Note: 'graph' needs to be defined elsewhere and should represent the compiled state graph for the chat agent.
+
+def chat_streaming_handler(thread_id : str, message : str) -> Iterator[str]:
+   """
+   Docstring for chat_streaming_handler
+   
+   :param thread_id: Description
+   :type thread_id: str
+   :param message: Description
+   :type message: str
+   """
+   for chunk , metadata in  graph.stream(
+      input = {
+         "messages" : [HumanMessage(content = message)]
+      },
+      config={
+         "configurable" : {
+            "thread_id" : thread_id
+         }
+      },
+         stream_mode = "messages"
+   ):
+      yield chunk.content
 
 def get_all_threads_handler():
    """
--- src/routes/chat_route.py
+++ src/routes/chat_route.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from src.handlers.chat_handler import chat_agent_handler
 from src.agents.chat_agent.states.chat_agent_state import ChatAgentState
-from src.handlers.chat_handler import get_all_threads_handler, chat_history_handler
+from src.handlers.chat_handler import get_all_threads_handler, chat_history_handler, chat_streaming_handler
+from fastapi.responses import StreamingResponse
 
 router = APIRouter()
 
@@ -16,6 +17,21 @@ def chat_agent_route(thread_id: str, message : str) -> ChatAgentState:
     :rtype: ChatAgentState
     """
     return chat_agent_handler(thread_id = thread_id, message = message )
+
+@router.post("/stream/{thread_id}")
+def chat_stream_route(thread_id : str, message : str):
+    """
+    Docstring for chat_stream_route
+    
+    :param thread_id: Description
+    :type thread_id: str
+    :param message: Description
+    :type message: str
+    """
+    return StreamingResponse(
+        chat_streaming_handler( thread_id , message ),
+        media_type="text/event-stream"
+    )
 
 @router.get("/chat/threads")
 def get_all_threads() -> list[str | None]:
